d490291687be66eefe905936507fb3df395ba5cd
mikolajce <mikolajce@gmail.com>
2022-08-11 14:38:11 +0200

    11.08.22 - koniec (?) konfigurowania środowisk, pierwsze testy bazy danych

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,15 +1,18 @@
 @extends ('temp')
 
+@section ('body')
+
 <form class="" action="/hello" method="post">
-  <p>login</p>
+  <p>Wprowadź login:</p>
   <br>
   <input type="text" name="" value="" placeholder="siemanokolano">
   <br>
-  <p>hasło</p>
+  <p>Wprowadź hasło:</p>
   <br>
   <input type="password" name="" value="" placeholder="pijemypiwo">
   <br>
   <!--<input type="submit" name="" value="Zaloguj">-->
-  <a href="/">Zaloguj</a>
-  <br>
+  <p><a href="/">Zaloguj</a></p>
 </form>
+
+@endsection
